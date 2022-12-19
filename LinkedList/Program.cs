930d69fc15389cced6e93b79a6754df85ae4a07f
unknown <abhimvj575@gmail.com>
2022-12-19 16:57:24 +0530

    [Abhilash] Added: create a LInkedList by adding 30,56 to 70

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,10 +5,12 @@ namespace LinkedList
     internal class Program
     {
         static void Main(string[] args)
-        { 
-            Node head = new Node(56);
-            head.next = new Node(30);
-            head.next.next = new Node(70);
+        {
+            linkedlist list = new linkedlist();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Display();
         }
     }
 }
--- LinkedList/linkedlist.cs
+++ LinkedList/linkedlist.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using LinkedList;
+using System;
 namespace LinkedList
 {
-    internal class linkedlist
+    class linkedlist
     {
         internal Node head;
         public linkedlist()
         {
             this.head = null;
+        }
+        internal void Add(int item)
+        {
+            Node node = new Node(item);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+        internal void Display()
+        {
+            if (head == null)
+                System.Console.WriteLine("List is Empty");
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
         }
     }
 }

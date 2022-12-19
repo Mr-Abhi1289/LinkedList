9519b93163127f4ba06141e1010495c7511009f3
unknown <abhimvj575@gmail.com>
2022-12-19 16:46:38 +0530

    [Abhilash] Added: Created Simple Linked List

---
+++ LinkedList/linkedlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class linkedlist
+    {
+        internal Node head;
+        public linkedlist()
+        {
+            this.head = null;
+        }
+    }
+}

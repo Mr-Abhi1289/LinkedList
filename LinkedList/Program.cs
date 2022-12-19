c121c35cc50c91c9f16282772325443e3a868baf
unknown <abhimvj575@gmail.com>
2022-12-19 22:01:45 +0530

    [Abhilash] Added: Insertion Between Two Numbers

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,9 +7,9 @@ namespace LinkedList
         static void Main(string[] args)
         {
             linkedlist list = new linkedlist();
-            list.Append(56);
-            list.Append(30);
+            list.Append(56);           
             list.Append(70);
+            list.Insert(2, 30);
             list.Display();
         }
     }
